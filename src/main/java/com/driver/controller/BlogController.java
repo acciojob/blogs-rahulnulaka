f880b7907324b3536145f806d36d380f5fcd1d7d
Nulaka Rahul <rahulsunny988@gmail.com>
2023-11-18 13:42:31 +0530

    Final Commit

--- src/main/java/com/driver/controller/BlogController.java
+++ src/main/java/com/driver/controller/BlogController.java
@@ -12,12 +12,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/blogs")
 public class BlogController {
+    @Autowired
+    private BlogService blogService;
 
     @PostMapping
     public ResponseEntity createBlog(@RequestParam Integer userId ,
                                      @RequestParam String title,
                                      @RequestParam String content) {
         // Create a blog and add it under given user
+        Blog blog=blogService.createAndReturnBlog(userId, title, content);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -17,15 +17,29 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
-
+        Blog blog=blogRepository2.findById(blogId).get();
+        Image image=new Image();
+        image.setBlog(blog);
+        image.setDescription(description);
+        image.setDimensions(dimensions);
+        imageRepository2.save(image);
+        return image;
     }
 
     public void deleteImage(Integer id){
-
+        imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
+        Image image=imageRepository2.findById(id).get();
+        String[] dim=image.getDimensions().split("X");
+        int inLen=Integer.parseInt(dim[0]);
+        int inWid=Integer.parseInt(dim[1]);
 
+        String[] dim2=screenDimensions.split("X");
+        int outLen=Integer.parseInt(dim2[0]);
+        int outWid=Integer.parseInt(dim2[1]);
+        return (outLen/inLen)*(outWid/inWid);
     }
 }
